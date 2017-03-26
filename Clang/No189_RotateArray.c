329b7790fcecb6c579f351216f756e2cf1d82e29
OopsMonk <oopsmonk@gmail.com>
2017-03-26 12:29:41 +0000

    C: No189, space complexity = O(1)

--- Clang/No189_RotateArray.c
+++ Clang/No189_RotateArray.c
@@ -2,6 +2,26 @@
 #include <string.h>
 #include <stdlib.h>
 
+#define _METHOD_ 1
+void dump(int *array, int size){
+    for(int i = 0; i < size; i++){
+        printf("%d,",array[i]);
+    }
+    printf("\n");
+}
+
+void array_reverse(int* nums, int size){
+    if(size <= 1)
+        return;
+
+    for(int i = 0; i < (size/2); i++){
+        int tmp = nums[i];
+        nums[i] = nums[size-i-1];
+        nums[size-i-1] = tmp;
+    }
+}
+
+#if _METHOD_ == 0
 // rotate to the right by 'k' steps. 
 void rotate(int* nums, int numsSize, int k) {
     
@@ -18,17 +38,33 @@ void rotate(int* nums, int numsSize, int k) {
     }
     
 }
+#elif _METHOD_ == 1
+// rotate to the right by 'k' steps.
+void rotate(int* nums, int numsSize, int k) {
+    dump(nums, numsSize);
+    //reverse all elements
+    array_reverse(nums, numsSize);
+    dump(nums, numsSize);
+    //reverse nums[0] ~ nums[k]
+    array_reverse(nums, k%numsSize);
+    dump(nums, numsSize);
+    //reverse nums[k+1] ~ nums[numssize]
+    array_reverse(nums+(k%numsSize), numsSize - (k%numsSize));
+    dump(nums, numsSize);
+}
+#endif
 
 int main(){
+#if _METHOD_ == 0 // space complexity = O(n), Runtime: 6 ms
     int a[7] = {1,2,3,4,5,6,7}; 
     // k = 3 then [5,6,7,1,2,3,4]
     int k = 5; 
-    int n = 7;
-    rotate(a, n, k);
-    for(int i = 0; i < n; i++){
-        printf("%d,",a[i]);
-    }
-    printf("\n");
-
+    rotate(a, 7, k);
+    dump(a,7);
+#elif _METHOD_ == 1 // space complexity = O(1), Runtime: 9 ms
+    int a[7] = {1,2,3,4,5,6,7};
+    int k = 10;
+    rotate(a, 7, k);
+#endif
     return 0;
 }
